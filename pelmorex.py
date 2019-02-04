37d1e1506cfc48d487fc2b6b8dfd7094d5b5c1ba
Jason Kendall <coolacid@gmail.com>
2019-02-03 22:22:08 -0500

    Clean up tests

--- pelmorex.py
+++ pelmorex.py
@@ -19,12 +19,19 @@ p = pynaads.naads(passhb=True)
 #    item = p.getQueue()
 #    if item:
 #        logger.debug(json.dumps(item))
+#        print(p.filter_in_geo(item, (44.389355, -79.690331)))
 
-#testdata = open("samples/6example_CAPCP_with_free_drawn_polygon.xml","r").read()  # Test A
-testdata = open("samples/014B55A1-6609-FCA2-BD91-A582E1EBCEF1.xml","r").read()     # Test B
-item = p.parse(testdata)
-#logger.debug(json.dumps(item))
+def testA():
+    testdata = open("samples/6example_CAPCP_with_free_drawn_polygon.xml","r").read()
+    item = p.parse(testdata)
+    print("Return Fail: {}".format(p.filter_in_geo(item, (72.02227211437801, -125.25787353515625))))
+    print("Return Return 1: {}".format(p.filter_in_geo(item, (51.5072466571743, -99.22714233398436))))
 
-# print(p.filter_in_geo(item, (44.389355, -79.690331)))
-print(p.filter_in_geo(item, (51.5072466571743, -99.22714233398436)))    # Matches Test A
-print(p.filter_in_geo(item, (72.02227211437801, -125.25787353515625)))  # Matches Test B
+def testB(): 
+    testdata = open("samples/014B55A1-6609-FCA2-BD91-A582E1EBCEF1.xml","r").read()
+    item = p.parse(testdata)
+    print("Should Fail: {}".format(p.filter_in_geo(item, (51.5072466571743, -99.22714233398436))))
+    print("Return Return 1: {}".format(p.filter_in_geo(item, (72.02227211437801, -125.25787353515625))))
+
+testA()
+testB()
