d5492c2ea6d3f007a742a52b526bde10856424ad
Jason Kendall <coolacid@gmail.com>
2019-02-12 20:01:09 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pynaads",
-    version="0.0.4",
+    version="0.1.0",
     author="VE3LSR / VE3YCA",
     author_email="projects@ve3lsr.ca",
     description="Python library to monitor Pelmorex's NAADS service",
