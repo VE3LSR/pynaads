29fc3ed35d1cab31d7e51f66eed3f03bc010aafa
Jason Kendall <coolacid@gmail.com>
2019-02-12 19:36:21 -0500

    Testing CLC tone test

--- pelmorex.py
+++ pelmorex.py
@@ -53,6 +53,13 @@ def test(file, pointA=None, pointB=None, codeA=None, codeB=None):
             print("Item Point: Should Fail: {}".format(p.filter_in_geo(q, pointA)))
             print("Item Point: Return Return 1: {}".format(p.filter_in_geo(q, pointB)))
         if codeA and codeB:
+            zones = {'018200': "1"}
+            if q['geocode']['layer:EC-MSC-SMC:1.0:CLC'][0] in zones:
+                zone = zones[q['geocode']['layer:EC-MSC-SMC:1.0:CLC'][0]]
+            else:
+                zone = ""
+            print (q['geocode']['layer:EC-MSC-SMC:1.0:CLC'][0])
+            print("{}: {} - {}".format(q['msgType'], q['areaDesc'], q['headline']), 'WXZ{}'.format(zone))
             print("Item Code: Should Return 1: {}".format(p.filter_in_clc(q, codeA)))
             print("Item Code: Should Fail: {}".format(p.filter_in_clc(q, codeB)))
             pass
@@ -63,8 +70,8 @@ def test(file, pointA=None, pointB=None, codeA=None, codeB=None):
 #test("samples/6example_CAPCP_with_free_drawn_polygon.xml", (72.02227211437801, -125.25787353515625), (51.5072466571743, -99.22714233398436))
 #test("samples/014B55A1-6609-FCA2-BD91-A582E1EBCEF1.xml", (51.5072466571743, -99.22714233398436), (72.02227211437801, -125.25787353515625))
 #test("samples/urn:oid:2.49.0.1.124.2651896163.2018.xml", pointA=(51.5072466571743, -99.22714233398436), pointB=(72.02227211437801, -125.25787353515625))
-#test("samples/urn:oid:2.49.0.1.124.2651896163.2018.xml", codeA='018200', codeB='999999')
-test("samples/urn:oid:2.49.0.1.124.2651896163.2018.xml", codeA=['018200'], codeB=['999999'])
+test("samples/urn:oid:2.49.0.1.124.2651896163.2018.xml", codeA='018200', codeB='999999')
+#test("samples/urn:oid:2.49.0.1.124.2651896163.2018.xml", codeA=['018200'], codeB=['999999'])
 #test("samples/707363CA-611B-BDDF-0494-A276829793D1.xml", (51.5072466571743, -99.22714233398436), (72.02227211437801, -125.25787353515625))
 #testAll()
 #run()
