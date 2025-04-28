55a54f2100e13917f6c469c3c22705915a78a5ac
Jason Kendall <jakendall@gmail.com>
2025-04-28 14:25:13 -0400

    Start actual tests

---
+++ test/conftest.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import pytest
+
+def getfile(file):
+    with open(file, "r") as f:
+        return f.read()
+
+@pytest.fixture
+def tester():
+    return getfile(Path(__file__).parent / "samples/Sample1_CAPCP_No_Attachment.xml")
