51330ecab115b5be5d147e5bde415c85f8a54817
Jason Kendall <jakendall@gmail.com>
2025-04-28 14:25:38 -0400

    Start actual tests

--- test/test_parse.py
+++ test/test_parse.py
@@ -1,4 +1,13 @@
-from pytest import fixture
+import pytest
+import pynaads
 
-def test_placeholder():
-    pass
+def test_placeholder(tester):
+    naads = pynaads.naads(passhb=True)
+    parsed = naads.parse(tester)
+    assert parsed.event['identifier'] == '78A038D9-701C-659D-47A8-7C54C13884C2'
+    assert parsed.event['msgType'] == 'Alert'
+    assert parsed.event['scope'] == 'Public'
+    assert parsed.event['sender'] == 'testSender@Pelmorex-test'
+    assert parsed.event['sent'] == '2018-04-13T09:35:16-04:00'
+    assert parsed.event['status'] == 'Actual'
+    assert parsed.event['info'] == []
